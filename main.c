9bf446e6a6b79e49b999dda710a8722efe5228ee
joahor <103511365+joahor@users.noreply.github.com>
2022-06-15 11:13:59 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -1,73 +1,76 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <ctype.h>
 
-int male_litery_nazwisko(char* s, int i);
-void test_male_litery_nazwisko();
-int macierz_nr_albumu(char** tabl, int n);
-void test_macierz_nr_albumu();
-void test_nazwisko_nr_albumu();
+#include "cwiczenia1.h"
+#include "cwiczenia2.h"
+#include "cwiczenia3.h"
+#include "cwiczenia4.h"
+#include "cwiczenia5.h"
+#include "cwiczenia6.h"
+#include "cwiczenia7.h"
+#include "cwiczenia8.h"
+#include "cwiczenia9.h"
+#include "cwiczenia10.h"
+#include "Kwiz.h"
+#include "kolokwiumA.h"
+
+void testy();
 
 int main()
 {
-	test_male_litery_nazwisko();
-	
-	return 0;
+    //cw1_zadanie1();
+    //cw1_zadanie2();
+    //cw1_zadanie3();
+    //cw1_zadanie4();
+    
+    //cw2_zadanie1();
+    //cw2_zadanie2();
+    //cw2_zadanie3();
+    //cw2_zadanie4();
+
+    //Kwiz1();
+    //CzyParzysta_Piotrek(47);
+    //TestCzyParzysta(34);
+    //TestWartoscBezwzgledna(34); //Nie ma funkcji WartoscBezwzglendna i nieskoñczony Test
+
+    //test_czy_potega_dwojki();
+    //test_postac_binarna();
+    //test_czy_doskonala();  
+
+    //test_generuj_tablice();
+    //test_elementy_parzyste_Piotrek();
+    //test_elementy_parzyste();
+  
+    //HorwacikJoanna();
+    //test_czy_automorficzna();
+    //test_czy_automorficzna2();
+    //test_czy_armstronga();
+    
+    //int b = reverse(234);
+    //printf("%d\n", b);
+    //int b = czy_palindrom(656);
+    //printf("%d\n", b);
+    //int b = odwroc_i_dodaj(195);
+    //printf("%d\n", b);
+
+    //test_bisekcja();
+    //test_liczba_pi();
+    //test_liczba_pi_mc();
+
+    //testy();
+
+    //test_lzespolone();
+    //czytaj_tekst_z_pliku1("Cwiczenia10.txt");
+    //czytaj_tekst_z_pliku2("Cwiczenia10.txt");
+    printf("%c",parzysta_cyfra_nierekurencyjnie(113));
+
+    return 0;
 }
 
-int male_litery_nazwisko(char* s, int i, int licznik)
+void testy()
 {
-	if (i >= strlen(s))
-	{
-		return licznik;
-	}
-	licznik += islower(s[1]) ? 1 : 0;
-	return male_litery_nazwisko(s, i+1, licznik);
-}
-void test_male_litery_nazwisko()
-{
-	char str[100] = "";
-	printf("Podaj wyraz: ");
-	scanf_s("%s", str, (int)sizeof(str));
-	printf("Podany wyraz to: %s\n", str);
-	printf("%d", male_litery_nazwisko(str, 0, 0));
-}
-
-int macierz_nr_albumu()
-{
-
-}
-
-void test_macierz_nr_albumu(char** tabl, int n)
-{
-	int a;
-	printf("podaj wymiar tablicy (n i a):");
-	if (scanf_s("%d %d", &n, &a) < 2) {
-		printf("niepoprawne dane");
-		return;
-	}
-	char** t = (char**)malloc(n * sizeof(char*));
-	if (t == NULL) {
-		printf("B³¹d alokacji pamieci\n");
-		return;
-	}
-}
-
-void test_nazwisko_nr_albumu()
-{
-	char imie[30] = "";
-	char nazwisko[60] = "";
-	unsigned int wiek;
-	char napis[100] = "";
-	printf("Podaj imie, nazwisko i wiek:\n");
-	if (scanf_s("%s %s %d",
-		imie, (int)sizeof(imie), nazwisko, (int)sizeof(nazwisko), &wiek) < 3)
-	{
-		printf("niepoprawne dane");
-		return;
-	}
-	sprintf_s(napis, sizeof(napis), imie);
-	sscanf_s(napis, "witaj %s %s [%d]", imie, nazwisko);
-	
+    int n = 5;
+    printf("%d!=%lld\n", n, nsilnia(n));
+    int a = 24, b = 40;
+    printf("NWD(%d,%d)=%d\n", a, b, nwd(a, b));
+    int fib = 8;
+    printf("Element %d ciagu fibonacciego = %d\n", fib, ciag_fibonacciego(fib));
 }
